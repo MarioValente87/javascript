4536efe217142ff59c19431216a65bdffc030fa0
MarioValente87 <mariovalente_uba@hotmail.com>
2022-03-14 23:31:13 -0300

    grandes modificaciones

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 /*Variables*/
 
-const btnCierra = document.querySelector("#btn-cierra");
-const btnAdelanta = document.querySelector("#btn-adelanta");
-const btnRetrocede = document.querySelector("#btn-retrocede");
-const imagenes = document.querySelectorAll("#galeria img");
-const lightbox = document.querySelector("#contenedor-principal");
-const imagenActiva = document.querySelector("#img-activa");
+const btnCierra = document.querySelector('#btn-cierra');
+const btnAdelanta = document.querySelector('#btn-adelanta');
+const btnRetrocede = document.querySelector('#btn-retrocede');
+const imagenes = document.querySelectorAll('#galeria img');
+const lightbox = document.querySelector('#contenedor-principal');
+const imagenActiva = document.querySelector('#img-activa');
 let indiceImagen = 0;
 
 /*Abre el Lightbox*/
 
 const abreLightbox = (event) => {
   imagenActiva.src = event.target.src;
-  lightbox.style.display = "flex";
+  lightbox.style.display = 'flex';
   indiceImagen = Array.from(imagenes).indexOf(event.target);
 };
 
 imagenes.forEach((imagen) => {
-  imagen.addEventListener("click", abreLightbox);
+  imagen.addEventListener('click', abreLightbox);
 });
 
 /*Cierra el Lightbox */
 
-btnCierra.addEventListener("click", () => {
-  lightbox.style.display = "none";
+btnCierra.addEventListener('click', () => {
+  lightbox.style.display = 'none';
 });
 
 /* Adelanta la imagen*/
@@ -36,7 +36,7 @@ const adelantaImagen = () => {
   indiceImagen++;
 };
 
-btnAdelanta.addEventListener("click", adelantaImagen);
+btnAdelanta.addEventListener('click', adelantaImagen);
 
 /*Retrocede la Imagen*/
 
@@ -48,31 +48,39 @@ const retrocederImagen = () => {
   indiceImagen--;
 };
 
-btnRetrocede.addEventListener("click", retrocederImagen);
+btnRetrocede.addEventListener('click', retrocederImagen);
 
 /* VENTANA MODAL */
-const modal = document.getElementById("modal");
-const overlay = document.getElementById("overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelector(".show-modal");
+
+/* VENTANA MODAL */
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelector('.show-modal');
 
 const openModal = () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  Swal.fire({
+    text: 'Hay muchisimo que podría contarte sobre mi, pero mejor es que lo veas por ti mismo! Sigueme en Instagram!',
+    backdrop: true,
+    allowOutsideClick: true,
+    allowEscapeKey: true,
+    allowEnterKey: true,
+    customClass: {
+      container: 'modal1',
+      popup: 'popup-class',
+    },
+    backdrop: `rgba(0,0,0,0.9)`,
+    buttonsStyling: true,
+    showCloseButton: true,
+    closeButtonAriaLabel: 'Cerrar alerta',
+    showConfirmButton: false,
+    showClass: {
+      popup: 'animate__bounceIn',
+    },
+    hideClass: {
+      popup: 'animate__bounceOut',
+    },
+  });
 };
-const closeModal = () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-btnsOpenModal.addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", (e) => {
-  console.log(e.key);
-  if (e.key === "Escape") {
-    if (!modal.classList.contains("hidden")) {
-      closeModal();
-    }
-  }
-});
+btnsOpenModal.addEventListener('click', openModal);
--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,201 @@
-"use strict";
+const productos = [
+  {
+    nombre: 'Pincel',
+    precio: 350,
+    imagen: '../productos/img/pincel.jpg',
+    id: 0,
+  },
+  {
+    nombre: 'celular',
+    precio: 250,
+    imagen: '../productos/img/celular.jpg',
+    id: 1,
+  },
+  {
+    nombre: 'Microfono',
+    precio: 450,
+    imagen: '../productos/img/microfono.jpg',
+    id: 2,
+  },
+];
 
-class Producto {
+const container = document.querySelector('.container');
+const main = document.querySelector('#main');
+const sidebar = document.querySelector('.sidebar');
+const btnCarrito = document.querySelector('.btn-carrito');
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+/* BOTON del carrito(la imagen del nav) con Swal */
+
+btnCarrito.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  if (carrito.length === 0) {
+    //para que aparezca un alert si haces click en el carrito y esta vacio
+    Swal.fire({
+      title: 'Carrito Vacio',
+      text: '🥲',
+      icon: 'warning',
+      background: '#333',
+    });
+  }
+});
+
+/* CARGA DE PRODUCTOS */
+const cargarProductos = () => {
+  productos.forEach((element) => {
+    main.innerHTML += ` <div class="caja">
+    <img class="caja--img" src="${element.imagen}">
+    <div class="caja--datos">
+    <p class="nombre">${element.nombre}</p>
+    <p class="precio">$ <span> ${element.precio}</span></p>
+    <button class="btn-agregar" data-id="${element.id}">Agregar</button>
+    </div>
+    </div>
+    `;
+  });
+  const btnAgregar = document.querySelectorAll('.btn-agregar');
+  btnAgregar.forEach((e) =>
+    e.addEventListener('click', (e) => {
+      let cardPadre = e.target.parentElement;
+      agregarAlCarrito(cardPadre);
+    })
+  );
+};
+//como vamos a usar varias veces la alerta, conviene tenerla en una función
+const swalToast = (texto, color, posicion) => {
+  Swal.fire({
+    toast: true,
+    text: texto,
+    background: color /* '#80ff80' */,
+    showConfirmButton: false,
+    position: posicion /* 'bottom-end' */,
+    timer: 1400,
+    timerProgressBar: true,
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown',
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp',
+    },
+  });
+};
+
+//Esta función "agregarAlCarrito" permite sacar la información de la card en formato de objeto al carrito.
+const agregarAlCarrito = (cardPadre) => {
+  swalToast('Producto Agregado', '#80ff80', 'bottom-end');
+  let producto = {
+    nombre: cardPadre.querySelector('.nombre').textContent,
+    precio: Number(cardPadre.querySelector('.precio span').textContent),
+    cantidad: 1,
+    imagen: cardPadre.parentElement.querySelector('img').src,
+    id: Number(cardPadre.querySelector('button').getAttribute('data-id')),
+  };
+  let productoEncontrado = carrito.find(
+    (element) => element.id === producto.id
+  );
+  if (productoEncontrado) {
+    productoEncontrado.cantidad++;
+  } else {
+    carrito.push(producto);
+  }
+  console.log(carrito);
+  mostrarCarrito();
+};
+
+const mostrarCarrito = () => {
+  sidebar.innerHTML = '  ';
+  carrito.forEach((element) => {
+    let { imagen, nombre, precio, cantidad, id } = element;
+    sidebar.innerHTML += `
+    <div class= "caja--carrito">
+            <img class="caja-carrito-img" src="${imagen}">
+            <div class="caja-carrito-datos">
+            <p class="nombre">${nombre}</p>
+            <p class="cantidad">CANTIDAD: ${cantidad}</p>
+            <p class="subtotal">Subtotal: $ ${precio * cantidad}</p>
+            <p class="precio">$<span>${precio}</span></p>
+            <button class="btn-restar" data-id="${id}">-</button>
+            <button class="btn-borrar" data-id="${id}">Borrar</button>
+           </div>
+      </div>
+    `;
+  });
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+  aumentarNumeroCantidadCarrito();
+};
+
+const restarProducto = (productoRestar) => {
+  swalToast('ProductoRetirado', '#ff99cc', 'bottom-end');
+  let productoEncontrado = carrito.find(
+    (element) => element.id === Number(productoRestar)
+  );
+  if (productoEncontrado) {
+    productoEncontrado.cantidad--;
+
+    if (productoEncontrado.cantidad === 0) {
+      productoEncontrado.cantidad = 1;
+    }
+  }
+  mostrarCarrito();
+};
+
+const borrarProducto = (productoBorrar) => {
+  carrito = carrito.filter((element) => element.id !== Number(productoBorrar));
+  mostrarCarrito();
+};
+
+const escucharBotonesSidebar = () => {
+  sidebar.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-restar')) {
+      restarProducto(e.target.getAttribute('data-id'));
+    }
+    if (e.target.classList.contains('btn-borrar')) {
+      borrarProducto(e.target.getAttribute('data-id'));
+    }
+  });
+};
+
+const aumentarNumeroCantidadCarrito = () => {
+  let total = carrito.reduce((acc, ite) => acc + ite.cantidad, 0);
+  document.querySelector('.cant--carrito').textContent = total;
+};
+
+cargarProductos();
+mostrarCarrito();
+escucharBotonesSidebar();
+/* const idCart = document.querySelector('#idCart'); */
+
+/* class Producto {
   constructor(producto, descripcion, precio) {
     this.producto = producto;
+    this.imagen;
     this.descripcion = descripcion;
     this.precio = precio;
   }
 }
 
 const producto1 = new Producto(
-  "pincel",
-  "Pincel de calidad premium para crear tus mejores obras de arte",
+  'pincel',
+  'Pincel de calidad premium para crear tus mejores obras de arte',
   400
 );
 const producto2 = new Producto(
-  "microfono",
-  "Si te gusta cantar, este producto es para vos",
+  'microfono',
+  'Si te gusta cantar, este producto es para vos',
   1000
 );
 const producto3 = new Producto(
-  "celular",
-  "Si te gusta crear contenido, este producto es para vos",
+  'celular',
+  'Si te gusta crear contenido, este producto es para vos',
   20000
 );
 
 const productos = [producto1, producto2, producto3];
-const divProductos = document.getElementById("idProductos2");
+const divProductos = document.getElementById('idProductos2');
 
 productos.forEach((productosEnArray, indice) => {
   divProductos.innerHTML += `<div class="card" id="productosEnArray${indice}" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${productosEnArray.producto}</h5>
     <p class="card-text">${productosEnArray.descripcion}</p>
@@ -41,12 +207,13 @@ productos.forEach((productosEnArray, indice) => {
 });
 let listaDeProductos = [];
 let agregarProducto = (indice) => {
-  let carrito = document.getElementById("idCarrito");
-  let nuevo = document.createElement("div");
-  nuevo.innerText = productos[indice].producto + " " + productos[indice].precio;
-  nuevo.classList.add("fondoAzul");
+  let carrito = document.getElementById('idCarrito');
+  let nuevo = document.createElement('div');
+  nuevo.innerText = productos[indice].producto + ' ' + productos[indice].precio;
+  nuevo.classList.add('fondoAzul');
   carrito.appendChild(nuevo);
   listaDeProductos.push(productos[indice]);
-  localStorage.setItem("carrito", JSON.stringify(listaDeProductos));
+  localStorage.setItem('carrito', JSON.stringify(listaDeProductos));
 };
+ */
 /* console.log(productos[indice].descripcion); */
