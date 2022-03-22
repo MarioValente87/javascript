877dfeb779411d6dba12bc5669f62c2b706f97ee
MarioValente87 <mariovalente_uba@hotmail.com>
2022-03-22 18:04:46 -0300

    update 1 index. Nueva libreria JS

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+/* SWIPER */
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
 /*Variables*/
 
 const btnCierra = document.querySelector('#btn-cierra');
@@ -7,50 +28,6 @@ const imagenes = document.querySelectorAll('#galeria img');
 const lightbox = document.querySelector('#contenedor-principal');
 const imagenActiva = document.querySelector('#img-activa');
 let indiceImagen = 0;
-
-/*Abre el Lightbox*/
-
-const abreLightbox = (event) => {
-  imagenActiva.src = event.target.src;
-  lightbox.style.display = 'flex';
-  indiceImagen = Array.from(imagenes).indexOf(event.target);
-};
-
-imagenes.forEach((imagen) => {
-  imagen.addEventListener('click', abreLightbox);
-});
-
-/*Cierra el Lightbox */
-
-btnCierra.addEventListener('click', () => {
-  lightbox.style.display = 'none';
-});
-
-/* Adelanta la imagen*/
-
-const adelantaImagen = () => {
-  if (indiceImagen === imagenes.length - 1) {
-    indiceImagen = -1;
-  }
-  imagenActiva.src = imagenes[indiceImagen + 1].src;
-  indiceImagen++;
-};
-
-btnAdelanta.addEventListener('click', adelantaImagen);
-
-/*Retrocede la Imagen*/
-
-const retrocederImagen = () => {
-  if (indiceImagen === 0) {
-    indiceImagen = imagenes.length;
-  }
-  imagenActiva.src = imagenes[indiceImagen - 1].src;
-  indiceImagen--;
-};
-
-btnRetrocede.addEventListener('click', retrocederImagen);
-
-/* VENTANA MODAL */
 
 /* VENTANA MODAL */
 const modal = document.getElementById('modal');
